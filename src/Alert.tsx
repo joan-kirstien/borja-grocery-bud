4a225224ade2f53d4dd2ad30864a59f1bbefd471
joan-kirstien <143709655+joan-kirstien@users.noreply.github.com>
2023-10-28 17:43:54 +0800

    Add files via upload

---
+++ src/Alert.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react'
+import { AlertData, ListData } from "./App";
+
+type AlertProps = AlertData & {
+  removeAlert: (
+    show?: boolean,
+    type?: "" | "success" | "danger",
+    msg?: string
+  ) => void;
+  list: ListData[];
+};
+
+export function Alert({msg, type,removeAlert, list}: AlertProps){
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [list, removeAlert]);
+
+  return <p className={`alert alert-${type}`}>{msg}</p>;
+}
